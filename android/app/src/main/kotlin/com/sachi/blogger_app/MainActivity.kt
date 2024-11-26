b7dca5692d244e90f7960d9631ba58359a953e9b
sachi-tmg <sachitamang43210@gmail.com>
2024-11-26 11:56:56 +0545

    initialization

---
+++ android/app/src/main/kotlin/com/sachi/blogger_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sachi.blogger_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:blogger_app/app.dart';
+import 'package:flutter/material.dart';
+
+// final
+// const
+
+// HOT RELOAD
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
